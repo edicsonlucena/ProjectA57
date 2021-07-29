79fa8e205c7086b69a1e751a0770ce1f48a78465
edicson.lucena@gmail.com <edicson.lucena@gmail.com>
2021-07-29 19:01:33 +0000

    Rename File and Add transition code

--- boston_housing.py
+++ boston_housing.py
@@ -11,13 +11,16 @@ pip install tensorflow
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_boston
 import mlflow.tensorflow
-
+import pandas as pd
 
 db = load_boston()
 
+boston = pd.DataFrame(db.data, columns=db.feature_names)
+
 X = db.data
 y = db.target
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=5)
+boston.head()
 
 
 # COMMAND ----------
@@ -47,77 +50,6 @@ with mlflow.start_run():
   predictions = rf.predict(X_test)
   
   
-
-# COMMAND ----------
-
-import mlflow
-import mlflow.sklearn
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
-
-# In this run, neither the experiment_id nor the experiment_name parameter is provided. MLflow automatically creates a notebook experiment and logs runs to it.
-# Access these runs using the Experiment sidebar. Click Experiment at the upper right of this screen. 
-with mlflow.start_run():
-  n_estimators = 100
-  max_depth = 6
-  max_features = 3
-  # Create and train model
-  rf = RandomForestRegressor(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features)
-  rf.fit(X_train, y_train)
-  # Make predictions
-  predictions = rf.predict(X_test)
-  
-  # Log parameters
-  mlflow.log_param("num_trees", n_estimators)
-  mlflow.log_param("maxdepth", max_depth)
-  mlflow.log_param("max_feat", max_features)
-  
-  # Log model
-  mlflow.sklearn.log_model(rf, "random-forest-model")
-  
-  # Create metrics
-  mse = mean_squared_error(y_test, predictions)
-    
-  # Log metrics
-  mlflow.log_metric("mse", mse)
-
-# COMMAND ----------
-
-# This run uses mlflow.set_experiment() to specify an experiment in the workspace where runs should be logged. 
-# If the experiment specified by experiment_name does not exist in the workspace, MLflow creates it.
-# Access these runs using the experiment name in the workspace file tree. 
-
-experiment_name = "/Shared/boston_housing_experiment/"
-mlflow.set_experiment(experiment_name)
-
-with mlflow.start_run():
-  n_estimators = 100
-  max_depth = 6
-  max_features = 3
-  # Create and train model
-  rf = RandomForestRegressor(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features)
-  rf.fit(X_train, y_train)
-  
-  run_id = mlflow.active_run().info.run_id
-    
-  # Make predictions
-  predictions = rf.predict(X_test)
-  
-  # Log parameters
-  mlflow.log_param("num_trees", n_estimators)
-  mlflow.log_param("maxdepth", max_depth)
-  mlflow.log_param("max_feat", max_features)
-  
-  # Log model
-  mlflow.sklearn.log_model(rf, "random-forest-model")
-  
-  # Create metrics
-  mse = mean_squared_error(y_test, predictions)
-    
-  # Log metrics
-  mlflow.log_metric("mse", mse)
-  
-
 
 # COMMAND ----------
 
@@ -287,6 +219,16 @@ def forecast_prix(model_name, model_stage):
 # COMMAND ----------
 
 forecast_prix(model_name, "Production")
+
+# COMMAND ----------
+
+client.transition_model_version_stage(
+  name=model_name,
+  version=2,
+  stage="Archived"
+)
+
+
 
 # COMMAND ----------
 
